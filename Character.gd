ccd11c5817362eecd3f4d9a3d8e53bf71c5fdc5a
Arnav-R-23 <arnavramteke20@gmail.com>
2025-07-11 22:03:03 +0530

    Add files via upload

---
+++ Character.gd
@@ -0,0 +1,13 @@
+class_name Character
+
+
+extends Node
+
+@export var profession : String
+@export var health     : int
+
+
+
+func die():
+	health = 0
+	print(profession + " died.")
---
+++ Main.gd
@@ -0,0 +1,8 @@
+extends Node
+
+@export var character_to_kill : Character
+
+func _ready():
+	character_to_kill.die()
+	
+
